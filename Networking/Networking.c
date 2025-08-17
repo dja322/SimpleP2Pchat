056f100a86ac2164605dff74d8212a8d6cbfe9ce
dja322 <dja322@nau.edu>
2025-08-17 10:44:20 -0700

    Move includes around for networking

--- Networking/Networking.c
+++ Networking/Networking.c
@@ -1,9 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <arpa/inet.h>
+
+#include "Networking.h"
 
 
 #define PORT 5000
--- Networking/Networking.h
+++ Networking/Networking.h
@@ -2,6 +2,12 @@
 #ifndef NETWORKING_H
 #define NETWORKING_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <arpa/inet.h>
 
 void* receive_thread(void* arg);
 int establish_connection();
